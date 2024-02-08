6b04571c297e723792c91ab06f99e4504dec4817
Utkarsh Prakash Singh <113572394+utkarshsingh2012@users.noreply.github.com>
2024-02-09 00:06:52 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (32.29%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
+                swap(mat[i][j], mat[j][i]);
                 if(i == j){
                     break;
                 }
-                else{
-                    swap(matrix[i][j], matrix[j][i]);
-                }
+                
             }
         }
-        
+       
         for(int i = 0; i<n; i++){
-            int low = 0, high = m-1;
-            while(low <= high){
-                swap(matrix[i][low], matrix[i][high]);
-                low++;
-                high--;
+            for(int j = 0; j<=(m-1)/2; j++){
+                swap(mat[i][j], mat[i][n-j-1]);
             }
+            
         }
     }
 };
