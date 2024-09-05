9efc3ab780b3e609f3ce8127f89a42888a0b806e
Utkarsh Prakash Singh <113572394+utkarshsingh2012@users.noreply.github.com>
2024-09-05 14:03:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (6.47%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
+    vector<int> res;
+    void print(vector<vector<int>>& matrix,int l ,int r ,int top ,int bottom){
+        for(int i = l ; i < r ; i++){
+            res.push_back(matrix[top][i]);
+        }
+        for(int i = top + 1; i < bottom ; i++){
+            res.push_back(matrix[i][r - 1]);
+        }
+        if(top == bottom - 1) return;
+        for(int i = r - 2; i >= l; i--){
+            res.push_back(matrix[bottom - 1][i]);
+        }
+        if(l == r- 1) return ;
+        for(int i = bottom - 2 ; i > top ; i--){
+            res.push_back(matrix[i][l]);
+        }
+        return;
+    }
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> vec;
-        int top = 0, bottom = n-1, left = 0, right = m-1;
-        while(top <= bottom && left <= right){
-            for(int i = left; i<= right; i++){
-                vec.push_back(matrix[top][i]);
-            }
+        int l = 0;
+        int r = m;
+        int top = 0;
+        int bottom = n;
+        while(top < bottom and l < r){
+            print(matrix,l,r,top,bottom);
+            l++;
+            r--;
             top++;
-            if(top > bottom) return vec;
-            for(int i = top; i<= bottom; i++){
-                vec.push_back(matrix[i][right]);
-            }
-            right--;
-            if(left > right)    return vec;
-            for(int i = right; i >= left; i--){
-                vec.push_back(matrix[bottom][i]);
-            }
             bottom--;
-            if(top > bottom) return vec;
-            for(int i = bottom; i >= top; i--){
-                vec.push_back(matrix[i][left]);
-            }
-            left++;
-            if(left > right)    return vec;
         }
-        return vec;
+        return res;
+
+        
+
+
+        
+
     }
 };
