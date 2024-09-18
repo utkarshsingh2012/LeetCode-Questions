d5f49dd4f6e15ba368b106be18125d6625ad05f8
Utkarsh Prakash Singh <113572394+utkarshsingh2012@users.noreply.github.com>
2024-09-18 15:45:19 +0530

    Time: 37 ms (59.55%), Space: 17.4 MB (24.99%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,38 +8,16 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode * currA = headA;
-        ListNode * currB = headB;
-        if(currA == currB) return currA;
-        set<ListNode * > st;
-        while(currA != NULL and currB != NULL){
-            if(currA == currB) return currA;
-            if(st.find(currB) != st.end()){
-                return currB;
-            }
-            if(st.find(currA) != st.end()){
-                return currA;
-            }
-            st.insert(currA);
-            st.insert(currB);
-            currA = currA -> next;
-            currB = currB -> next;
+     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* tempA=headA;
+        ListNode* tempB=headB;
+        while(tempA!=tempB){
+            tempA=tempA->next;
+            tempB=tempB->next;
+            if(tempA==tempB){ return tempA;}
+            if(tempA==NULL){ tempA=headB;}
+            else if(tempB==NULL){ tempB=headA;}
         }
-        while(currA != NULL){
-            if(st.find(currA) != st.end()){
-                return currA;
-            }
-            st.insert(currA);
-            currA = currA -> next;
-        }
-        while(currB != NULL){
-            if(st.find(currB) != st.end()){
-                return currB;
-            }
-            st.insert(currB);
-            currB = currB -> next;
-        }
-        return NULL;
+        return tempA;
     }
 };
